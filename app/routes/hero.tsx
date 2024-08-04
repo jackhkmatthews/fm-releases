50d0af7cfd9034f0f00fe845ae095e4b539d4bdf
Jack Matthews <jackhkmatthews@gmail.com>
2024-08-04 14:12:51 +0100

    fix(hero): update copy

--- app/routes/hero.tsx
+++ app/routes/hero.tsx
@@ -9,12 +9,11 @@ export function Hero() {
         "bg-gray-950 flex flex-col pt-16 pb-10"
       )}
     >
-      <h1 className="w-full text-gray-50 font-sans leading-tight text-[max(1.875rem,8cqw)] translate-x-[-0.1ch]">
+      <h1 className="w-full text-gray-50 font-sans leading-tight text-[max(2rem,8cqw)] translate-x-[-0.1ch]">
         Fresh Meta Releases
       </h1>
       <p className="text-gray-300">
-        A curated selection of books, films, music and art. Released, promoted
-        and distributed by{" "}
+        A curated selection of books, films, music and art released on{" "}
         <a
           href="https://www.metalabel.com/"
           className="underline"
